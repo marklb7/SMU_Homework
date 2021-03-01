ed361ba0110a6ca8c3583460187c18c1a928f7dc
marklb7 <mark_96@sbcglobal.net>
2021-03-01 00:05:20 -0600

    Mapping HW init

---
+++ 17-Mapping-Web/Submission/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoibWFya2xiMzciLCJhIjoiY2trZzVzZzRuMDJybDMxcTl1dGV6cnEwZCJ9.FCcLZewygJ1BOyK092ky-g";
---
+++ 17-Mapping-Web/Submission/static/js/logic.js
@@ -0,0 +1,187 @@
+$(document).ready(function() {
+    makeMap();
+});
+
+function makeMap() {
+    // data
+    // Store our API endpoint as queryUrl
+
+    var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
+    var tectonicPlates = "static/data/tetonic_plates.json"
+        // Perform a GET request to the query URL
+    $.ajax({
+        type: "GET",
+        url: queryUrl,
+        success: function(data) {
+            $.ajax({
+                type: "GET",
+                url: tectonicPlates,
+                success: function(tectonic) {
+
+                    buildMap(data, tectonic);
+                },
+                error: function(XMLHttpRequest, textStatus, errorThrown) {
+                    alert("Status: " + textStatus);
+                    alert("Error: " + errorThrown);
+                }
+            });
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            alert("Status: " + textStatus);
+            alert("Error: " + errorThrown);
+        }
+    });
+}
+
+function buildMap(data, tectonic) {
+
+    // Step 0: Create the Tile Layers
+    // Add a tile layer
+    var dark_mode = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/dark-v10",
+        accessToken: API_KEY
+    });
+
+    var light_mode = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/light-v10",
+        accessToken: API_KEY
+    });
+
+    // STEP 1: INIT MAP
+    // Create a map object
+    var myMap = L.map("map", {
+        center: [37.0902, -95.7129],
+        zoom: 5,
+        layers: [light_mode, dark_mode]
+    });
+
+    var earthquakes = [];
+    var circle_list = [];
+    data.features.forEach(function(earthquake) {
+        var marker = L.geoJSON(earthquake, {
+            onEachFeature: onEachFeature
+        });
+        earthquakes.push(marker);
+
+        var circle = L.geoJSON(earthquake, {
+            pointToLayer: function(feature, latlng) {
+                var geojsonMarkerOptions = createMarkerOptions(feature)
+
+                return L.circleMarker(latlng, geojsonMarkerOptions);
+            },
+            onEachFeature: onEachFeature
+        });
+        circle_list.push(circle);
+    });
+    var tectonic_plates = L.geoJSON(tectonic, {
+        color: "green",
+        weight: 2
+    });
+
+
+    var marker_group = L.layerGroup(earthquakes);
+    var marker_group2 = L.layerGroup(circle_list);
+    var tectonic_layer = L.layerGroup([tectonic_plates]);
+
+    // Create Layer Legend
+    var baseMaps = {
+        "Light Mode": light_mode,
+        "Dark Mode": dark_mode
+    };
+
+    var overlayMaps = {
+        "Markers": marker_group,
+        "Circles": marker_group2,
+        "Tectonic Plates": tectonic_layer
+    };
+
+    // Slap Layer Legend onto the map
+    L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+
+    marker_group2.addTo(myMap);
+    tectonic_plates.addTo(myMap);
+
+    // Set up the legend
+    var legend = L.control({ position: "bottomright" });
+    legend.onAdd = function() {
+        var div = L.DomUtil.create("div", "info legend");
+
+        // Create the legend
+        var legendInfo = `<h2 style = "margin-bottom:10px"> Earthquake Depth </h2>
+        <div>
+        <div style = "background:#0095EF;height:10px;width:10px;display:inline-block"> </div> 
+        <div style = "display:inline-block"> Less than 10 Miles</div>
+        </div> 
+        <div>
+        <div style = "background:#3C50B1;height:10px;width:10px;display:inline-block"></div> 
+        <div style = "display:inline-block">10 - 30 Miles</div>
+        </div>
+        <div>
+        <div style = "background:#6A38B3;height:10px;width:10px;display:inline-block"></div>
+        <div style = "display:inline-block">30 - 50 Miles</div>
+        </div>
+        <div>
+        <div style = "background:#A224AD;height:10px;width:10px;display:inline-block"></div> 
+        <div style = "display:inline-block">50 - 70 Miles</div>
+        </div>
+        <div>
+        <div style = "background:#F31D64;height:10px;width:10px;display:inline-block"></div>
+        <div style = "display:inline-block">70 - 90 Miles</div>
+        </div> 
+        <div>
+        <div style = "background:#FE433C;height:10px;width:10px;display:inline-block"></div>
+        <div style = "display:inline-block">Greater than 90 Miles</div>
+        </div>`;
+
+        div.innerHTML = legendInfo;
+        return (div)
+    }
+
+    // Adding legend to the map
+    legend.addTo(myMap);
+
+}
+
+function createMarkerOptions(feature) {
+
+    var depth = feature.geometry.coordinates[2];
+    var depthColor = "";
+    if (depth > 90) {
+        depthColor = "#FE433C";
+    } else if (depth > 70) {
+        depthColor = "#F31D64";
+    } else if (depth > 50) {
+        depthColor = "#A224AD";
+    } else if (depth > 30) {
+        depthColor = "#6A38B3";
+    } else if (depth > 10) {
+        depthColor = "#3C50B1";
+    } else {
+        depthColor = "#0095EF";
+    }
+    var geojsonMarkerOptions = {
+        radius: (feature.properties.mag * 5) + 1,
+        fillColor: depthColor,
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    return (geojsonMarkerOptions)
+}
+
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    if (feature.properties && feature.properties.title && feature.geometry && feature.geometry.coordinates) {
+        layer.bindPopup(`<h3>${feature.properties.title}<br>Magnitude: ${feature.properties.mag}<br>Depth: ${feature.geometry.coordinates[2]} miles</h3>`);
+    }
+}
